c3d6b890ee9a97bb6d746a1da72fe41f140421c5
Vunnix <victor.alvesfariasx@gmail.com>
2016-05-27 17:55:37 -0400

    Commit na nova versão do Android Studio!

--- AprendendoAndroid/app/src/main/java/com/example/vunix/aprendendoandroid/MainActivity.java
+++ AprendendoAndroid/app/src/main/java/com/example/vunix/aprendendoandroid/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.vunix.aprendendoandroid;
 
+import android.content.Context;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText tfLogin;
+    private EditText tfsenha;
+    final Context context =  this;
+    Button btEntrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btEntrar = (Button) findViewById(R.id.btEntrar);
+        btEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.print("Clicou no botão");
+                AlertDialog.Builder alert = new AlertDialog.Builder(context).setMessage("Clicou no botão");
+            }
+        });
+        }
     }
-}
