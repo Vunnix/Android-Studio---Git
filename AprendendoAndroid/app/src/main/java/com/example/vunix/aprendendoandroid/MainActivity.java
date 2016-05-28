f9272b6707adebf7c00ddf0d006193253fcba95d
Vunnix <victor.alvesfariasx@gmail.com>
2016-05-28 10:00:46 -0400

    Terceiro teste de commit no Android Studio.2.1

--- AprendendoAndroid/app/src/main/java/com/example/vunix/aprendendoandroid/MainActivity.java
+++ AprendendoAndroid/app/src/main/java/com/example/vunix/aprendendoandroid/MainActivity.java
@@ -17,5 +17,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btEntrar = (Button) findViewById(R.id.btEntrar);
+        btEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         }
     }
